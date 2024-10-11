7c072471c9c1a6f4eaf91356e7323a458c1c617c
Mateo Oviedo <mateosebastian159@gmail.com>
2024-10-10 21:26:38 -0500

    deberes: 03 - add

---
+++ java/deberes/03/Deber03/src/Resta.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Resta {
+    public static void main(String[] args) {
+        int a,b;
+        System.out.println("Bienvenido a la calculadora de resta by Mateo Oviedo y Alex Travez");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el primer número: ");
+        a = sc.nextInt();
+        System.out.println("Introduce el segundo número: ");
+        b = sc.nextInt();
+        System.out.println("Resultado: " + a + " - " + b + " = " + (a-b));
+
+        int c= 1;
+        boolean valor = true;
+        valor = c == 1 ? true : c== 2 ? false : false;
+    }
+}
