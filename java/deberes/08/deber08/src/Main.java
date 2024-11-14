710344a2d0231329bf1229e00d3fdb7d7f2b6526
alextravez563 <alextravez563@gmail.com>
2024-11-14 11:34:00 -0500

    practica: 05 - update, remove unused files and classes related to the card game implementation

--- java/deberes/08/deber08/src/Main.java
+++ java/deberes/08/deber08/src/Main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+//jsjsjsjjajkjaksajskjkaksjkakjaksksakajk
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
--- java/practicas/05/practica05/src/Repartidor.java
+++
@@ -1,4 +0,0 @@
-public interface Repartidor {
-    void barajar();         // Método para mezclar las cartas
-    Carta21 repartirCarta(); // Método para repartir una carta
-}
