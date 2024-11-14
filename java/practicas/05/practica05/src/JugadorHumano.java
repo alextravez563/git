08947c98bd07646f16b1d407fa5c923a87e074a6
alextravez563 <alextravez563@gmail.com>
2024-11-14 11:26:25 -0500

    practica: 05 - update, add card game implementation with classes for Baraja, Carta, and Juego21

--- java/practicas/05/practica05/src/JugadorHumano.java
+++ java/practicas/05/practica05/src/JugadorHumano.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+public class JugadorHumano extends Jugador {
+    public JugadorHumano(String nombre) {
+        super(nombre);
+    }
 
-public class JugadorHumano {
+    // Implementación del cálculo de puntaje para el jugador humano
+    @Override
+    public int calcularPuntaje() {
+        int puntaje = 0;
+        int cantAs = 0;
+
+        // Sumar los puntos de cada carta en la mano
+        for (Carta21 carta : mano) {
+            puntaje += carta.getPuntos();
+            if (carta.getValor().equals("A")) {
+                cantAs++;
+            }
+        }
+
+        // Ajuste para Ases si el puntaje supera 21
+        while (puntaje > 21 && cantAs > 0) {
+            puntaje -= 10;
+            cantAs--;
+        }
+
+        return puntaje;
+    }
 }
--- java/practicas/05/practica05/src/Repartidor.java
+++ java/practicas/05/practica05/src/Repartidor.java
@@ -1,4 +1,4 @@
-package PACKAGE_NAME;
-
-public class Repartidor {
+public interface Repartidor {
+    void barajar();         // Método para mezclar las cartas
+    Carta21 repartirCarta(); // Método para repartir una carta
 }
